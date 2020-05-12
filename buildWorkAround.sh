25c1745d1a6a375f0301c1c14b7465a068c14c16
GlitchWraith <GlitchWraith@ghostlink.net>
2020-05-12 21:16:55 +0100

    Remove API Key Usage

--- buildWorkAround.sh
+++ buildWorkAround.sh
@@ -4,6 +4,6 @@ set collection [lindex $argv 0]
 set API [lindex $argv 1]
 set timeout -1
 
-spawn /home/steam/Server1/srcds_run -game garrysmod +maxplayers 2 +map gm_flatgrass +gamemode terrortown -console +host_workshop_collection $collection -authkey $API 
+spawn /home/steam/Server1/srcds_run -game garrysmod +maxplayers 2 +map gm_flatgrass +gamemode terrortown -console +host_workshop_collection $collection
 
 expect "VAC secure mode is activated." {close}

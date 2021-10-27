945237fe4a42375eec82d5b86eaa3453a2c9b008
Harvey Baxter <wraith@ghostlink.net>
2021-10-27 17:23:29 +0000

    Update buildWorkAround.sh, Dockerfile, server.cfg

--- buildWorkAround.sh
+++ buildWorkAround.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env expect 
 set scards [lindex $argv 0]
 set collection [lindex $argv 1]
-set API [lindex $argv 2]
 set timeout -1
 
 spawn $scards -game garrysmod +maxplayers 2 +map gm_flatgrass +gamemode terrortown -console +host_workshop_collection $collection
